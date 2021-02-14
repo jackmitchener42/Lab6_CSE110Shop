d5bdfeb2d4134475f0348b3bc1cd902a6f1bce69
jackmitchener42 <49843668+jackmitchener42@users.noreply.github.com>
2021-02-13 19:34:50 -0800

    complete the lab

--- components/product-item.js
+++ components/product-item.js
@@ -2,6 +2,72 @@
 
 class ProductItem extends HTMLElement {
   // TODO
+  constructor() {
+    super();
+
+    let shadow = this.attachShadow({
+      mode: 'open'
+    });
+
+    const styleBoi = document.createElement('link');
+    styleBoi.setAttribute('rel', 'stylesheet');
+    styleBoi.setAttribute('href', '../styles/styles.css');
+
+    let card = document.createElement('span');
+    card.className = 'product';
+
+    let img = card.appendChild(document.createElement('img'));
+    img.className = 'img';
+
+    let title = card.appendChild(document.createElement('p'));
+    title.className = 'title';
+
+    let price = card.appendChild(document.createElement('p'));
+    price.className = 'price';
+
+    let button = card.appendChild(document.createElement('button'));
+    button.className = 'button';
+    button.addEventListener('click', function (event) {
+
+      event.preventDefault();
+      var cart = JSON.parse(window.localStorage.getItem('cart'));
+
+      if (button.innerText == 'add to cart') {
+        button.innerText = 'remove from cart';
+        cart[this.id - 1] = 1;
+        document.getElementById('cart-count').textContent++;
+        alert('Added to Cart');
+      }
+
+      else {
+        button.innerText = 'add to cart';
+        cart[this.id - 1] = 0;
+        document.getElementById('cart-count').textContent--;
+        alert('Removed from Cart');
+      }
+
+      window.localStorage.setItem('cart', JSON.stringify(cart));
+    });
+
+    shadow.appendChild(card);
+    shadow.appendChild(styleBoi);
+
+  }
+
+  assign(prod, prod_val) {
+    this.id = prod.id;
+    var card = this.shadowRoot.firstChild.childNodes;
+    card[0].src = prod.image;
+    card[0].alt = prod.description;
+    card[1].innerText = prod.title;
+    card[2].innerText = '$' + prod.price;
+    card[3].id = prod.id;
+    card[3].innerText = 'add to cart';
+    if (prod_val) {
+      card[3].innerText = 'remove from cart';
+      document.getElementById('cart-count').textContent++;
+    }
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,26 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  if(window.localStorage.getItem('data') == undefined) {
+    fetch('https://fakestoreapi.com/products').then(response =>response.json()).then(data =>{
+    window.localStorage.setItem('data',JSON.stringify(data));
+  })
+}
+
+var products = JSON.parse(window.localStorage.getItem('data'));
+
+var cart = new Array(products.length).fill(0);
+
+if(window.localStorage.getItem('cart') == undefined){
+  window.localStorage.setItem('cart', JSON.stringify(cart));
+}
+else{
+  cart = JSON.parse(window.localStorage.getItem('cart'));
+}
+var product_list = document.getElementById('product-list');
+for(var i = 0; i < products.length; i++){
+  var item = product_list.appendChild(document.createElement('product-item'));
+  //could use other built in functions or change function name
+  item.assign(products[i], cart[i]);
+}
 });
